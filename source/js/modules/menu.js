d956da34af372dc3f9ca39c5bcf94e53ebc1d518
Andrey Matveev <andrei_matveev1250@mail.ru>
2023-12-02 20:27:12 +0600

    Завершает хедер

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,13 +1,15 @@
 const navMain = document.querySelector('[data-nav="header"]');
 const navToggle = navMain.querySelector('[data-button="header"]');
 
+navMain.classList.remove('is-nojs');
+
 function menuScript() {
   navToggle.addEventListener('click', () => {
     if (navMain.classList.contains('is-close')) {
       navMain.classList.remove('is-close');
       navMain.classList.add('is-open');
     } else {
-      navMain.classList.add('is-closed');
+      navMain.classList.add('is-close');
       navMain.classList.remove('is-open');
     }
   });
