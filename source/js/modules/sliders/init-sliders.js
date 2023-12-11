884103dfe3d1ba94ef7d9bfd88c14a3dda9f8154
Andrey Matveev <andrei_matveev1250@mail.ru>
2023-12-12 01:22:06 +0600

    Настраивает слайдер блока обучения

--- source/js/modules/sliders/init-sliders.js
+++ source/js/modules/sliders/init-sliders.js
@@ -1,9 +1,11 @@
 import {initSliderHero} from './hero';
 import {initSliderTours} from './tours';
+import {initSliderTraining} from './training';
 
 const initSliders = () => {
   initSliderHero();
   initSliderTours();
+  initSliderTraining();
 };
 
 export {initSliders};
---
+++ source/js/modules/sliders/training.js
@@ -0,0 +1,37 @@
+const slider = document.querySelector('[data-swiper="training"]');
+const buttonNext = document.querySelector('[data-button="next-training"]');
+const buttonPrev = document.querySelector('[data-button="prev-training"]');
+
+const setSlider = () => {
+  return new window.Swiper(slider, {
+    navigation: {
+      nextEl: buttonNext,
+      prevEl: buttonPrev,
+    },
+
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+
+      320: {
+        slidesPerView: 1,
+        initialSlide: 2,
+      },
+    },
+  });
+};
+
+const initSliderTraining = () => {
+  if (document.body.contains(slider)) {
+    setSlider();
+  }
+};
+
+export {initSliderTraining};
