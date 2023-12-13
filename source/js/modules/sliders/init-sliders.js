6e39b4095b27c4c1f2e077f583ce144135282e25
Andrey Matveev <andrei_matveev1250@mail.ru>
2023-12-14 01:45:45 +0600

    Заканчиваю блок отзывов

--- source/js/modules/sliders/init-sliders.js
+++ source/js/modules/sliders/init-sliders.js
@@ -1,11 +1,13 @@
 import {initSliderHero} from './hero';
 import {initSliderTours} from './tours';
 import {initSliderTraining} from './training';
+import {initSliderReviews} from './reviews';
 
 const initSliders = () => {
   initSliderHero();
   initSliderTours();
   initSliderTraining();
+  initSliderReviews();
 };
 
 export {initSliders};
